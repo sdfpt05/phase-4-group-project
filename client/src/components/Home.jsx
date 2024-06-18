7aa7c33663c2fba0e9047942fd96be6133bc6910
Sonia <sonia@Sonias-MacBook-Pro.local>
2024-06-18 13:51:35 +0300

    changes

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './css/Home.css'
+
+const Home = () => {
+    return (
+        <div className='hero'>
+            <div className="hero-text">Book Shop</div>
+            <p className='hero-description'>
+                Browse the collection of our best top interesting Books.
+                you will definitely find what you are looking for.
+            </p>
+            <div className='hero-image'></div>
+        </div>
+    )
+}
+
+export default Home
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Books from './components/Books'
+import { Signup } from './components/Signup'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path= '/' element={<App />}/>
+      <Route path= '/books' element={<Books />}/>
+      <Route path= '/Signup' element={<Signup />}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
