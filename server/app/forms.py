d59cf1aec399f11eb27ee0bf87bf23f5b3f97fb9
OchiengJr <137082048+OchiengJr@users.noreply.github.com>
2024-06-21 16:00:38 +0300

    Update forms.py

--- server/app/forms.py
+++ server/app/forms.py
@@ -3,15 +3,4 @@ from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-class BookForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    author = StringField('Author', validators=[DataRequired()])
-    description = TextAreaField('Description', validators=[Length(min=0, max=500)])
-    submit = SubmitField('Submit')
+    
