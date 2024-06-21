b54dd081def14a610428d28f48fbc395b87da7a2
OchiengJr <137082048+OchiengJr@users.noreply.github.com>
2024-06-21 16:01:24 +0300

    Update forms.py

--- server/app/forms.py
+++ server/app/forms.py
@@ -3,4 +3,5 @@ from wtforms import StringField, PasswordField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class SignupForm(FlaskForm):
+      username = StringField('Username', validators=[DataRequired()])
     
