24e87b8c775681c95f07230f2a3c1416d947d65a
kevin <kevin@novek.io>
2024-06-21 19:24:22 +0300

    feat:fixed db connections

--- server/app/config.py
+++ server/app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://phase_4_project:password@localhost/phase_4_app_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'postgresql://phase_4_project:password@localhost/phase_4_app_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
