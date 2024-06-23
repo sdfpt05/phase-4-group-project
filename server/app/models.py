06529a820722346be770ed455d4e46bb544ce3d5
reaganmunene <reagannmunene@gmail.com>
2024-06-23 12:37:00 +0300

    added Book model

--- server/app/models.py
+++ server/app/models.py
@@ -1,1 +1,15 @@
 from . import db
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), index=True, unique=True)
+    author = db.Column(db.String(120), index=True)
+    description = db.Column(db.String(500))
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'author': self.author,
+            'description': self.description
+        }
