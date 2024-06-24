c69fd2845206a9ef75548ed76ecda75eac4f24cc
kevin <kevin@novek.io>
2024-06-24 20:23:11 +0300

    feat:added flask CORS

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .config import Config
+from flask_cors import CORS
+
 
 db = SQLAlchemy()
 migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    # app.config.from_object(Config)
+    cors=CORS(app)   
+    app.config["CORS_HEADERS"] = "Content-Type" 
 
     db.init_app(app)
     migrate.init_app(app, db)
